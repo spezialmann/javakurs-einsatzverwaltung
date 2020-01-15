ebf021c28b50004c3f5c1a7db57680693a344bfb
MarcoT <spezialmann@me.com>
2020-01-15 17:58:33 +0100

    Reset

--- src/main/java/com/abi/einsatzplanung/domain/EinsatzGeber.java
+++ src/main/java/com/abi/einsatzplanung/domain/EinsatzGeber.java
@@ -9,28 +9,7 @@ public class EinsatzGeber<ContentType> extends List {
 
 
     public void fuegeEin(Einsatz pEinsatz) {
-        if(pEinsatz!=null) {
-            if(this.isEmpty()) {
-                this.append(pEinsatz);
-            }
-            else {
-                //Zeiger auf ersten Eintrag
-                this.toFirst();
-                Einsatz current = (Einsatz)this.getContent();
-                //Iteration bis Prio neue Aktivitaet kleiner als Prio Eintrag aus Liste
-                while (this.hasAccess() && (pEinsatz.getPPrioritaet() <= current.getPPrioritaet()) ) {
-                    this.next();
-                }
-                //Wenn ein Eintrag auf diesem Listenplatz, dann davor einfuegen
-                if(this.hasAccess()) {
-                    this.insert(pEinsatz);
-                }
-                //Sonst als neuen letzten Eintrag anfuegen
-                else {
-                    this.append(pEinsatz);
-                }
-            }
-        }
+        this.append(pEinsatz);
     }
 
     public Einsatz gibNaechstenEinsatz() {
